5e0396e7a143272d0a8d3daa90668ee55f1a4eb3
Samsarabbi Suborno <35795592+Samsarabbi007@users.noreply.github.com>
2019-05-29 02:38:31 +0600

    home

---
+++ VoteOnline/Home/footer.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <footer class="footer" style="width: 35%; margin:auto;">
+      <a href="#"><i class="fa fa-facebook-official" style="padding: 20px"></i></a>
+      <a href="#"><i class="fa fa-pinterest-p" style="padding: 20px"></i></a>
+      <a href="#"><i class="fa fa-twitter" style="padding: 20px"></i></a>
+      <a href="#"><i class="fa fa-flickr" style="padding: 20px"></i></a>
+      <a href="#"><i class="fa fa-linkedin" style="padding: 20px"></i></a>
+    </footer>    
+</div>
+
+
+</body>
+</html>
---
+++ VoteOnline/Home/index.php
@@ -0,0 +1,50 @@
+<?php 
+  require 'header.php';
+?>
+
+<div class="container"><h2 align="center">Welcome to Online Voting System</h2><br></div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-5 request">
+            <div id="demo" class="carousel slide" data-ride="carousel">
+              <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+              </ul>
+              <!-- The slideshow -->
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="carousel/votingVoting11.jpg" >
+                </div>
+                <div class="carousel-item">
+                  <img src="carousel/voting22.jpg" >
+                </div>
+                <div class="carousel-item">
+                  <img src="carousel/voting.jpg" >
+                </div>
+              </div>
+
+              <!-- Left and right controls -->
+              <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+              </a>
+              <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+              </a>
+          </div>
+        </div>         
+      <div class="col-sm-6 updates">
+           <h1>Online Election</h1>
+           <h3>If you want to arrange an election, please sign up as Election commissioner</h3>
+      </div>
+  </div>
+</div>
+
+
+
+<?php
+  require 'footer.php';
+?>
+
